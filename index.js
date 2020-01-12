f15cd7d4a09900e45350730e6b7b452ce7ef1e72
sash stasyk <sashman90@gmail.com>
2020-01-12 00:41:42 +0000

    Generating clients and basic servier up and running with a basic query

---
+++ index.js
@@ -0,0 +1,17 @@
+const { prisma } = require('./generated/prisma-client')
+const { GraphQLServer } = require('graphql-yoga')
+
+const resolvers = {
+  Query: {
+    customers: (root, args, context) => context.prisma.customers()
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs: './schema.graphql',
+  resolvers,
+  context: {
+    prisma,
+  },
+})
+server.start(() => console.log('Server is running on http://localhost:4000'))
